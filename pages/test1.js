2a276fb4769fa349801d6df98ddc5e501eaf8500
Kevin <korlando7@gmail.com>
2017-02-17 09:50:32 -0500

    test update

--- pages/test1.js
+++ pages/test1.js
@@ -5,6 +5,9 @@ var commands = {
     return this
       .waitForElementVisible('@landingVideo')
       .verify.containsText('@mainHeader', 'We are Red Ventures.')
+      .getText('@content2', function (res) {
+        console.log(res.value);
+      })
   }
 }
 
@@ -13,7 +16,8 @@ module.exports = {
   url: 'https://www.redventures.com/',
   elements: {
     landingVideo: 'div.cover_all',
-    mainHeader: 'h1.hero_heading'
+    mainHeader: 'h1.hero_heading',
+    content2: 'h2.content-block__heading'
   },
   commands: [commands]
 }
