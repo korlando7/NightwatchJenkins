bed8eb6632ed3677dbb8200db1e54848eb4b7023
Kevin <korlando7@gmail.com>
2017-02-21 16:53:49 -0500

    test update1000

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -10,8 +10,8 @@ module.exports = {
   "selenium": { // downloaded by selenium-download module (see readme)
     "start_process": true, // tells nightwatch to start/stop the selenium process
     "server_path": "./bin/selenium.jar",
-    "host": "127.0.0.1",
-    "port": 5556,  // standard selenium port
+    "host": "http://ec2-54-174-251-152.compute-1.amazonaws.com:8080/",
+    "port": 4444,  // standard selenium port
     "cli_args": { // chromedriver is downloaded by selenium-download (see readme)
       "webdriver.chrome.driver" : "./bin/chromedriver"
     }
