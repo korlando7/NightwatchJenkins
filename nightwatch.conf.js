26703fae36525787607a24525cf09191ceba7d5f
Kevin <korlando7@gmail.com>
2017-02-21 16:52:32 -0500

    test update1000

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -11,7 +11,7 @@ module.exports = {
     "start_process": true, // tells nightwatch to start/stop the selenium process
     "server_path": "./bin/selenium.jar",
     "host": "127.0.0.1",
-    "port": 4444,  // standard selenium port
+    "port": 5556,  // standard selenium port
     "cli_args": { // chromedriver is downloaded by selenium-download (see readme)
       "webdriver.chrome.driver" : "./bin/chromedriver"
     }
